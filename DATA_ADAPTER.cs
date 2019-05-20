564310dd0391d2a8388f6d57dd54b33382ff4e23
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-05-20 07:15:34 +0300

    Create DATA_ADAPTER.cs

---
+++ DATA_ADAPTER.cs
@@ -0,0 +1,43 @@
+private static DataSet GetData()
+        {
+            try
+            {
+                SqlConnection oConn = new SqlConnection("Data Source=svdsql17;Initial Catalog=DBBM;Integrated Security=SSPI ; User Id=DBBM_USER;Password=DBBM_USER; MultipleActiveResultSets=true");
+
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = new SqlCommand();
+                    da.SelectCommand.Connection = oConn;
+                    da.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    da.SelectCommand.CommandText = "SP_TENDER_ASPOSE";
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@TENDER", "199"));  // 2475
+
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "Clauses");
+
+                    return ds;
+
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error: " + ex.Message);
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return null;
+            }
+        }
+
+...............
+
+DataTable dt = ds.Tables["Clauses"];
+foreach (DataRow record in dt.Rows)
+            {
+                DoSomething(..........., record);
+            }
+
+
